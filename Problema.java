88dc4c1f2c365dae19f15775f823eaa1004a1082
MatiasValencia <144842386+MatiasValencia@users.noreply.github.com>
2023-10-24 19:50:48 +0000

    Sin terminar

---
+++ Problema.java
@@ -0,0 +1,27 @@
+public class Problema {
+    //Atributos
+    public static void main(String[] args) {
+        String[] listaAFP = new String[4];
+        listaAFP[0] = "Modelo";
+        listaAFP[1] = "Habitat";
+        listaAFP[2] = "Cuprum";
+        listaAFP[3] = "Capital";
+        String[] listaSalud = new String[4];
+        listaSalud[0] = "Fonasa";
+        listaSalud[1] = "Masvida";
+        listaSalud[2] = "Banmedica";
+        listaSalud[3] = "Colmena";
+        double[] descAFP = new double[4];
+        descAFP[0] = 0.07;
+        descAFP[1] = 0.1;
+        descAFP[2] = 0.12;
+        descAFP[3] = 0.08;
+        double[] descSalud = new double[4];
+        descSalud[0] = 0.07;
+        descSalud[1] = 0.15;
+        descSalud[2] = 0.10;
+        descSalud[3] = 0.11;
+
+        
+    }
+}
